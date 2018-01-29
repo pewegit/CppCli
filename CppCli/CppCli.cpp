18f98797a6fd8d978e868cfe763304d33bef36a9
pewegit <pewegit@gmx.de>
2018-01-30 00:40:16 +0100

    # array access added # timestamp added

--- CppCli/CppCli.cpp
+++ CppCli/CppCli.cpp
@@ -2,10 +2,34 @@
 
 #include "stdafx.h"
 
+#include <windows.h> // SYSTEMTIME
+
 using namespace System;
 
 int main(array<System::String ^> ^args)
 {
     Console::WriteLine(L"Hello World");
+
+	array<String^> ^arr = gcnew array<String^>(10);
+	int i = 0;
+
+	for each(String^% s in arr) {
+		s = i++.ToString();
+		Console::WriteLine(s);
+	}
+
+	Console::WriteLine("Array-Value: {0}", arr[5]);
+
+	SYSTEMTIME st, lt;
+
+	GetSystemTime(&st);
+	GetLocalTime(&lt); // considers summer or winter time
+
+	String^ strTimeStamp = lt.wYear.ToString("0000") + "-" + lt.wMonth.ToString("00") + "-" + lt.wDay.ToString("00")
+		+ " " + lt.wHour.ToString("00") + ":" + lt.wMinute.ToString("00") + ":" + lt.wSecond.ToString("00") + "." + lt.wMilliseconds.ToString("000");
+	Console::WriteLine(strTimeStamp);
+	Console::WriteLine("Time-Stamp [{0}:{1,-2}:{2,2}:{3,2}:{4,2}:{5,2}.{6,3}]", lt.wYear, lt.wMonth, lt.wDay, lt.wHour, lt.wMinute, lt.wSecond, lt.wMilliseconds);
+
+
     return 0;
 }
