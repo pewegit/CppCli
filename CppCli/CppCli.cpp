033a138537305c77d8a1d881f8042be83b2bad7b
pewegit <pewegit@gmx.de>
2018-02-01 00:01:26 +0100

    test_heapApi extended

--- CppCli/CppCli.cpp
+++ CppCli/CppCli.cpp
@@ -6,10 +6,25 @@
 
 using namespace System;
 
+void test_cliSyntax(void);
+void test_systemTime(void);
+void test_heapApi(void);
+
 int main(array<System::String ^> ^args)
 {
     Console::WriteLine(L"Hello World");
 
+	test_cliSyntax();
+
+	test_systemTime();
+
+	test_heapApi();
+	
+    return 0;
+}
+
+void test_cliSyntax(void)
+{
 	array<String^> ^arr = gcnew array<String^>(10);
 	int i = 0;
 
@@ -19,7 +34,10 @@ int main(array<System::String ^> ^args)
 	}
 
 	Console::WriteLine("Array-Value: {0}", arr[5]);
+}
 
+void test_systemTime(void)
+{
 	SYSTEMTIME st, lt;
 
 	GetSystemTime(&st); // UTC-based (does not consider DST = daylight saving time)
@@ -29,7 +47,10 @@ int main(array<System::String ^> ^args)
 		+ " " + lt.wHour.ToString("00") + ":" + lt.wMinute.ToString("00") + ":" + lt.wSecond.ToString("00") + "." + lt.wMilliseconds.ToString("000");
 	Console::WriteLine(strTimeStamp);
 	Console::WriteLine("Time-Stamp [{0}:{1,-2}:{2,2}:{3,2}:{4,2}:{5,2}.{6,3}]", lt.wYear, lt.wMonth, lt.wDay, lt.wHour, lt.wMinute, lt.wSecond, lt.wMilliseconds);
+}
 
+void test_heapApi(void)
+{
 	// Heap API check
 	typedef struct
 	{
@@ -40,12 +61,13 @@ int main(array<System::String ^> ^args)
 		double value3;
 	} T_myType;
 	HANDLE hHeap = HeapCreate(HEAP_NO_SERIALIZE, 1000, 1000000);
-	T_myType *p = (T_myType *) HeapAlloc(hHeap, HEAP_NO_SERIALIZE, 10000000); // too large
+
+	//double *p = (double *)HeapAlloc(hHeap, HEAP_NO_SERIALIZE, 100);
+	//p[1000] = -17.42; // throws access violation
+
+	T_myType *p = (T_myType *)HeapAlloc(hHeap, HEAP_NO_SERIALIZE, 10000000); // too large
 	p += 0x12345678;
 	p[17].value3 = -17.42; // throws access violation
-
+	
 	BOOL retVal = HeapDestroy(hHeap);
-
-
-    return 0;
 }
